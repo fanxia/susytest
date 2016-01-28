b0c022abc1a01a1efaa0315a4046adba3b978b06
fanxia <fanxia08@gmail.com>
2016-01-28 17:17:30 +0100

    trg

--- testTrigger.py
+++ testTrigger.py
@@ -5,28 +5,38 @@
 
 import ROOT
 from ROOT import *
-fl=TFile.Open('root://xrootd-cms.infn.it//store/group/phys_smp/ggNtuples/13TeV/data/V07_04_14_00/GoldenJSON/job_data_ggNtuple_SingleMuon_Run2015D_PromptReco-v4_25ns_JSON_Golden_1560pb_miniAOD.root')
+#fl=TFile.Open('root://xrootd-cms.infn.it//store/group/phys_smp/ggNtuples/13TeV/data/V07_04_14_00/GoldenJSON/job_data_ggNtuple_SingleMuon_Run2015D_PromptReco-v4_25ns_JSON_Golden_1560pb_miniAOD.root')
+fl=TFile.Open('data/SingleElectron_Run2015D_PromptReco-v4_25ns_JSON_Silver_1915pb_miniAOD__data_example.root')
 eventTree=fl.Get('ggNtuplizer/EventTree')
+
+Hltelemu=ROOT.TH1F('hlt','x',100,0,100)
 
 nEvent=eventTree.GetEntriesFast()
 print "nEvent=",nEvent
 n=0
 trg=[]
-a=2**53-1
-for i in range(0,1000):
+#a=2**53-1
+for i in range(0,100):
     eventTree.GetEntry(i)
-    a&=eventTree.HLTEleMuX
-#    print "---xxxx---",eventTree.HLTEleMuX
-#    a=bin(eventTree.HLTEleMuX)[2:][::-1]
-#    for j in range(len(a)):
-#        if a[j]=='1':
-#            trg.append(j)
-#    trg=list(set(trg))
+#    a&=eventTree.HLTEleMuX
+    print "---xxxx---",eventTree.HLTEleMuX
+    a=bin(eventTree.HLTEleMuX)[2:][::-1]
+    for j in range(len(a)):
+        if a[j]=='1':
+            Hltelemu.Fill(j)
+
+            trg.append(j)
+            trg=list(set(trg))
         
 print "---------------------------------------"
 print "EventNumber = ",i
 #print trg
-print bin(a)
+#print bin(a)
+
+c=ROOT.TCanvas("c","Plots",800,800)
+c.cd()
+Hltelemu.Draw()
+c.Print("trigger","pdf")
 '''
 c=ROOT.TCanvas("c","Plots",800,800)
 c.cd()
